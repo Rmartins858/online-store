0f44aeeea06c55726df1f0c5aa01d3f8bd02c2d7
Rafael Martins <rmartins858@gmail.com>
2022-05-24 20:12:13 -0300

    requisito 5 ...

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,58 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import CategorieButton from '../components/CategorieButton';
-import { getCategories } from '../services/api';
+import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
 
 class Home extends React.Component {
   state = {
     categories: [],
+    searchList: [],
+    search: [],
+    xablau: '',
+
   }
 
   componentDidMount() {
     this.fetchCategories();
   }
 
+  onInputChange= ({ target }) => {
+    const { value } = target;
+    this.setState({
+      search: value,
+    });
+  }
+
+  listageral= () => {
+    const { search } = this.state;
+    const test =  getProductsFromCategoryAndQuery(undefined, search);
+    this.setState({
+      searchList: test.results,
+    })
+
   fetchCategories = async () => {
     const categories = await getCategories();
     this.setState({ categories });
   }
 
-  render() {
-    const { categories } = this.state;
+  render(){
+    const { categories, search } = this.state;
     console.log(categories);
 
     return (
       <div>
-        <input type="text" />
+        <input
+          type="text"
+          value={ search }
+          data-testid="query-input"
+          onChange={ this.onInputChange }
+        />
+
+        <button
+          type="button"
+          data-testid="query-button"
+          onClick={ this.listageral }
+        />
         <h1 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h1>
