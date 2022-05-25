06245db4ca92cdf783e6d274216603978d81f813
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2022-05-25 18:38:56 -0300

    Finaliza requisito 7

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -5,6 +5,7 @@ import { getProductsFromID } from '../services/api';
 class ProductDetails extends React.Component {
     state ={
       products: {},
+      attributes: [],
     }
 
     componentDidMount= async () => {
@@ -13,36 +14,35 @@ class ProductDetails extends React.Component {
 
     searchProduct = async () => {
       const { match } = this.props;
-      const test = await getProductsFromID(match.params.id);
+      const productAPI = await getProductsFromID(match.params.id);
       this.setState({
-        products: test,
+        products: productAPI,
       });
-      console.log(test);
+    }
+
+    productsDet = () => {
+      const { products } = this.state;
+      const { attributes } = products;
+      const detailsAttribute = attributes.map((i) => i);
+      this.setState({
+        attributes: detailsAttribute,
+      });
     }
 
     render() {
-      const { products } = this.state;
-      const { attributes } = products;
-      console.log(attributes);
+      const { products, attributes } = this.state;
       return (
         <div name={ products.id } data-testid="product">
           <img src={ products.thumbnail } alt={ products.title } />
           <p data-testid="product-detail-name">{products.title}</p>
           <p>{products.price}</p>
-          {/* {products > 0 && (
-            <div>
-              {attributes.length > 0 && (
-                attributes.map((i) => (
-                  <p key={ i.value_id }>
-                    {' '}
-                    test
-                  </p>
-
-                ))
-              )}
-
-            </div>
-          )} */}
+          <ul>
+            {attributes.map((i) => (
+              <li key={ i.id }>
+                {`${i.name}: ${i.value_name}`}
+              </li>
+            ))}
+          </ul>
         </div>
       );
     }
