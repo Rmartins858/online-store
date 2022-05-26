65ab717b373369b78822717efebb07e0998126dd
Bruno Riwerson Silva <riwerson@gmail.com>
2022-05-26 15:54:21 -0300

    Adicionando item ao carrinho a partir da página ProductDetails (Requisito 9)

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { getProductsFromID } from '../services/api';
 
@@ -6,10 +7,17 @@ class ProductDetails extends React.Component {
     state ={
       product: {},
       attributes: [],
+      cartItems: [],
     }
 
     componentDidMount= async () => {
       await this.searchProduct();
+      await this.getCartItemsFromLocalStorage();
+    }
+
+    getCartItemsFromLocalStorage = () => {
+      const cartItems = JSON.parse(localStorage.getItem('cartItems'));
+      this.setState({ cartItems });
     }
 
     searchProduct = async () => {
@@ -21,6 +29,18 @@ class ProductDetails extends React.Component {
         product: productAPI,
         attributes,
       });
+    }
+
+    saveCartItemsInLocalStorage = () => {
+      const { cartItems } = this.state;
+      localStorage.setItem('cartItems', JSON.stringify(cartItems));
+    }
+
+    addProductToCart = () => {
+      const { product, cartItems } = this.state;
+      this.setState((prevState) => ({
+        cartItems: cartItems ? [...prevState.cartItems, product] : [product],
+      }), this.saveCartItemsInLocalStorage);
     }
 
     render() {
@@ -37,6 +57,14 @@ class ProductDetails extends React.Component {
               </li>
             ))}
           </ul>
+          <button
+            type="button"
+            data-testid="product-detail-add-to-cart"
+            onClick={ this.addProductToCart }
+          >
+            Adicionar ao Carrinho
+          </button>
+          <Link to="/shopping-cart" data-testid="shopping-cart-button">Carrinho</Link>
         </div>
       );
     }
