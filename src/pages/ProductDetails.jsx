6847859f6b99c5e6ed669c09a3365ff92832bab4
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2022-05-26 15:01:41 -0300

    Questão 11 funciona mas não passa no teste

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -6,11 +6,16 @@ class ProductDetails extends React.Component {
     state ={
       products: {},
       attributes: [],
+      email: '',
+      message: '',
+      nota: '',
+      avaliations: [],
     }
 
     componentDidMount= async () => {
       await this.searchProduct();
       await this.productsDet();
+      await this.getAvaliationFromLocalStorage();
     }
 
     searchProduct = async () => {
@@ -30,13 +35,51 @@ class ProductDetails extends React.Component {
       });
     }
 
+    onRadioButtonClick = ({ target }) => {
+      const { id } = target;
+      this.setState({
+        nota: id,
+      });
+    }
+
+    handleChange = ({ target }) => {
+      const { name, value } = target;
+      this.setState({
+        [name]: value,
+      });
+    }
+
+    printAvaliation = () => {
+      const { email, message, nota, products: { id } } = this.state;
+      const avaliation = { email, message, nota, id };
+      this.setState((prevState) => ({
+        avaliations: [...prevState.avaliations, avaliation],
+      }), () => this.saveAvaliationInLocalStorage());
+    }
+
+    saveAvaliationInLocalStorage = () => {
+      const { avaliations } = this.state;
+      localStorage.setItem('avaliation', JSON.stringify(avaliations));
+    }
+
+    getAvaliationFromLocalStorage = () => {
+      const retornoLocalStorageJson = localStorage.getItem('avaliation');
+      const retornoLocalStorageObj = JSON.parse(retornoLocalStorageJson);
+      if (retornoLocalStorageObj !== null) {
+        this.setState({
+          avaliations: retornoLocalStorageObj,
+        });
+      }
+    }
+
     render() {
-      const { products, attributes } = this.state;
+      const { products, attributes, avaliations } = this.state;
+      const { match: { params: { id } } } = this.props;
       return (
         <div name={ products.id } data-testid="product">
           <img src={ products.thumbnail } alt={ products.title } />
-          <p data-testid="product-detail-name">{products.title}</p>
-          <p>{products.price}</p>
+          <h1 data-testid="product-detail-name">{products.title}</h1>
+          <h2>{products.price}</h2>
           <ul>
             {attributes.map((i) => (
               <li key={ i.id }>
@@ -44,6 +87,90 @@ class ProductDetails extends React.Component {
               </li>
             ))}
           </ul>
+          <div>
+            <h1>Avaliações</h1>
+            <form>
+              <input
+                type="email"
+                placeholder="Insira seu email"
+                data-testid="product-detail-email"
+                name="email"
+                onChange={ this.handleChange }
+                autoComplete="off"
+              />
+              <label htmlFor="1">
+                <input
+                  type="radio"
+                  name="nota"
+                  id="1"
+                  data-testid="1-rating"
+                  onClick={ this.onRadioButtonClick }
+                />
+                1
+              </label>
+              <label htmlFor="2">
+                <input
+                  type="radio"
+                  name="nota"
+                  id="2"
+                  data-testid="2-rating"
+                  onClick={ this.onRadioButtonClick }
+                />
+                2
+              </label>
+              <label htmlFor="3">
+                <input
+                  type="radio"
+                  name="nota"
+                  id="3"
+                  data-testid="3-rating"
+                  onClick={ this.onRadioButtonClick }
+                />
+                3
+              </label>
+              <label htmlFor="4">
+                <input
+                  type="radio"
+                  name="nota"
+                  id="4"
+                  data-testid="4-rating"
+                  onClick={ this.onRadioButtonClick }
+                />
+                4
+              </label>
+              <label htmlFor="5">
+                <input
+                  type="radio"
+                  name="nota"
+                  id="5"
+                  data-testid="5-rating"
+                  onClick={ this.onRadioButtonClick }
+                />
+                5
+              </label>
+              <textarea
+                placeholder="Mensagem(opcional)"
+                data-testid="product-detail-evaluation"
+                onChange={ this.handleChange }
+                name="message"
+              />
+              <button
+                type="button"
+                data-testid="submit-review-btn"
+                onClick={ this.printAvaliation }
+              >
+                Avaliar
+              </button>
+            </form>
+          </div>
+          {avaliations.filter((element) => element.id === id)
+            .map((element, index) => (
+              <div key={ index }>
+                <p>{ element.email }</p>
+                <p>{ element.nota }</p>
+                <p>{ element.message }</p>
+              </div>
+            ))}
         </div>
       );
     }
