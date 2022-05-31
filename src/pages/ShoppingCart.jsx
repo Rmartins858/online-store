6d2af8898beda6c9e1185994f808f4faed5d0902
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2022-05-31 11:40:12 -0300

    Requisito 14

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -2,101 +2,110 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class ShoppingCart extends Component {
-  state = {
-    cartItems: [],
+state = {
+  cartItems: [],
+}
+
+componentDidMount = async () => {
+  await this.getCartItemsFromLocalStorage();
+}
+
+getCartItemsFromLocalStorage = () => {
+  const cartItems = JSON.parse(localStorage.getItem('cartItems'));
+  this.setState({ cartItems });
+}
+
+plusQuantity = ({ target }) => {
+  const { cartItems } = this.state;
+  const unselectedItems = cartItems.filter((item) => item.id !== target.name);
+  const selectedItem = cartItems.find((object) => object.id === target.name);
+
+  if (selectedItem.quantity >= selectedItem.available_quantity) {
+    this.setState({ cartItems: [selectedItem, ...unselectedItems] },
+      localStorage.setItem('cartItems', JSON.stringify(cartItems)));
   }
 
-  componentDidMount = async () => {
-    await this.getCartItemsFromLocalStorage();
-  }
-
-  getCartItemsFromLocalStorage = () => {
-    const cartItems = JSON.parse(localStorage.getItem('cartItems'));
-    this.setState({ cartItems });
-  }
-
-  plusQuantity = ({ target }) => {
-    const { cartItems } = this.state;
-    const unselectedItems = cartItems.filter((item) => item.id !== target.name);
-    const selectedItem = cartItems.find((object) => object.id === target.name);
+  if (selectedItem.quantity < selectedItem.available_quantity) {
     selectedItem.quantity += 1;
-
-    this.setState({ cartItems: [...unselectedItems, selectedItem] },
+    this.setState({ cartItems: [selectedItem, ...unselectedItems] },
       localStorage.setItem('cartItems', JSON.stringify(cartItems)));
   }
+}
 
-  minusQuantity = ({ target }) => {
-    const { cartItems } = this.state;
-    const unselectedItems = cartItems.filter((item) => item.id !== target.name);
-    const selectedItem = cartItems.find((object) => object.id === target.name);
-    selectedItem.quantity -= 1;
+minusQuantity = ({ target }) => {
+  const { cartItems } = this.state;
+  const unselectedItems = cartItems.filter((item) => item.id !== target.name);
+  const selectedItem = cartItems.find((object) => object.id === target.name);
 
-    this.setState({ cartItems: [...unselectedItems, selectedItem] },
-      localStorage.setItem('cartItems', JSON.stringify(cartItems)));
-  }
+  this.setState((prevState) => ({
+    cartItems: [...unselectedItems, selectedItem],
+    quantidade: prevState.quantidade - 1,
+  }),
+  localStorage.setItem('cartItems', JSON.stringify(cartItems)));
+}
 
-  render() {
-    const { cartItems } = this.state;
-    return (
-      <div>
-        {!cartItems ? (
-          <h2
-            data-testid="shopping-cart-empty-message"
-          >
-            Seu carrinho está vazio
-          </h2>
-        ) : (
-          <div>
-            {cartItems && (
-              cartItems.map((item) => (
-                <div key={ item.id }>
-                  <img
-                    src={ item.thumbnail }
-                    alt={ item.title }
-                    style={ { width: '7cm' } }
-                  />
-                  <h2 data-testid="shopping-cart-product-name">{ item.title }</h2>
-                  <p style={ { fontWeight: 'bold' } }>
-                    Preço: R$
-                    {' '}
-                    <span>{ item.price * item.quantity }</span>
+render() {
+  const { cartItems } = this.state;
+  return (
+    <div>
+      {!cartItems ? (
+        <h2
+          data-testid="shopping-cart-empty-message"
+        >
+          Seu carrinho está vazio
+        </h2>
+      ) : (
+        <div>
+          {cartItems && (
+            cartItems.map((item) => (
+              <div key={ item.id }>
+                <img
+                  src={ item.thumbnail }
+                  alt={ item.title }
+                  style={ { width: '7cm' } }
+                />
+                <h2 data-testid="shopping-cart-product-name">{ item.title }</h2>
+                <p style={ { fontWeight: 'bold' } }>
+                  Preço: R$
+                  {' '}
+                  <span>{ item.price * item.quantity }</span>
+                </p>
+                <div style={ { display: 'flex' } }>
+                  Quantidade:
+                  {' '}
+                  <p data-testid="shopping-cart-product-quantity">
+                    { item.quantity }
                   </p>
-                  <div style={ { display: 'flex' } }>
-                    Quantidade:
-                    {' '}
-                    <p data-testid="shopping-cart-product-quantity">
-                      { item.quantity }
-                    </p>
-                    <button
-                      type="button"
-                      data-testid="product-increase-quantity"
-                      onClick={ this.plusQuantity }
-                      name={ item.id }
-                      style={ { margin: '5px' } }
-                    >
-                      +
+                  <button
+                    type="button"
+                    data-testid="product-increase-quantity"
+                    onClick={ this.plusQuantity }
+                    name={ item.id }
+                    style={ { margin: '5px' } }
+                  >
+                    +
 
-                    </button>
-                    <button
-                      type="button"
-                      data-testid="product-decrease-quantity"
-                      onClick={ this.minusQuantity }
-                      name={ item.id }
-                      style={ { margin: '5px' } }
-                    >
-                      -
+                  </button>
+                  <button
+                    type="button"
+                    data-testid="product-decrease-quantity"
+                    onClick={ this.minusQuantity }
+                    name={ item.id }
+                    style={ { margin: '5px' } }
+                  >
+                    -
 
-                    </button>
-                  </div>
+                  </button>
                 </div>
-              ))
-            )}
-          </div>
-        )}
-        <Link to="checkout" data-testid="checkout-products">Finalizar Compra</Link>
-      </div>
-    );
-  }
+              </div>
+            ))
+          )}
+        </div>
+      )}
+      <Link to="checkout" data-testid="checkout-products">Finalizar Compra</Link>
+    </div>
+  );
+}
 }
 
 export default ShoppingCart;
